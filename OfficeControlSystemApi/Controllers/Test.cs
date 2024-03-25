cd56d24e4a7845bbf821d09c727700891789810d
Victor-Senishyn <senishin2004@gmail.com>
2024-03-25 03:01:23 +0200

    Update

---
+++ OfficeControlSystemApi/Controllers/Test.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OfficeControlSystemApi.Controllers
+{
+    public class Test : Controller
+    {
+        [HttpGet("/test")]
+        public IActionResult Test1()
+        {
+            return Ok("HelloWorld");
+        }
+    }
+}
